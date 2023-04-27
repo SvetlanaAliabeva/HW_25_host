32c58dc3560d257258b30a1beb13ba4d8daf52b1
S_Kri <s_krivosheya@mail.ru>
2023-04-27 15:50:38 +0900

    fix

--- run.py
+++ run.py
@@ -1,15 +1,7 @@
-# from app import create_app
-#
-# app = create_app()
-#
-#
-# app.debug = True
-# if __name__ == '__main__':
-# 	app.run()create
-
-
 from flask import Flask
+
 from app import create_app
+
 app: Flask = create_app()
 
 if __name__ =='__main__':
